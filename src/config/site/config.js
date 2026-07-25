e34f74fd2d9e4cd22a24e93cca1aa36bf60c22af
andres <aotercarb@gmail.com>
2026-07-25 19:12:23 +0100

    Nombre web corregido

--- src/config/site/config.js
+++ src/config/site/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // The origin of the website (without trailing slash)
   origin: 'https://andres-otero.github.io',
   // The base pathname of the website
-  basePathname: '/',
+  basePathname: '/bodaalbaabel',
   // If the website uses trailing slashes in the URLs
   trailingSlash: false,
 
