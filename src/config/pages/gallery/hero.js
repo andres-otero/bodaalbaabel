c290359b9ac0ce87c6e01c72fc879d1d6e87535b
Andrés Otero Carballal <aotercarb@gmail.com>
2026-08-20 19:21:49 +0200

    prueba

--- src/config/pages/gallery/hero.js
+++ src/config/pages/gallery/hero.js
@@ -53,7 +53,7 @@ const hero = {
     // If the image should be enabled or not
     enabled: false,
     // Image source
-    src: import('~/assets/images/hero.png'),
+    src: import('~/assets/images/1.jpeg'),
     // Image alt text
     alt: 'Grant and Leah',
   },
